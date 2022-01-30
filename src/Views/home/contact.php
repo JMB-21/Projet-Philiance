48a77022f6f0deecc50d0163093a70f97a248e21
fridom99 <92981202+fridom99@users.noreply.github.com>
2022-01-30 16:18:26 +0100

    update contact

--- src/Views/home/contact.php
+++ src/Views/home/contact.php
@@ -15,76 +15,47 @@
             <form id="FormContact" method="post" enctype="multipart/form-data">
                 <div class="row row contact">
                     <div class="col-md-6">
-                        <div class="col-md-12">
+
                             <div class="input-group input-group-contact">
-                                <span class="input-group-addon input-group-addon-contact">Nom</span>
                                 <input type="text" name="nom" class="form-control" id="nom" placeholder="Nom" required="">
-                            </div>
+
                         </div>
-                        <div class="col-md-12">
+
                             <div class="input-group input-group-contact">
-                                <span class="input-group-addon input-group-addon-contact">Prénom</span>
                                 <input type="text" name="prenom" class="form-control" id="nom" placeholder="Prénom" required="">
                             </div>
-                        </div>
-                        <div class="col-md-12">
+
+
                             <div class="input-group input-group-contact">
-                                <span class="input-group-addon input-group-addon-contact">Sociéte</span>
                                 <input type="text" name="societe" class="form-control" id="societe" placeholder="Prénom" required="">
                             </div>
-                        </div>
-                        <div class="col-md-12">
+
                             <div class="input-group input-group-contact">
-                                <span class="input-group-addon input-group-addon-contact">Téléphone</span>
                                 <input type="text" name="societe" class="form-control" id="tel" placeholder="Téléphone" required="">
                             </div>
-                        </div>
+
                     </div>
                     <div class="col-md-6">
-                        <div class="col-md-12">
+
                             <div class="input-group input-group-contact">
-                                <span class="input-group-addon input-group-addon-contact">Adresse</span>
                                 <input type="text" name="adresse" class="form-control" id="adresse" placeholder="Adresse" required="">
                             </div>
-                        </div>
-                        <div class="col-md-12">
+
                             <div class="input-group input-group-contact">
-                                <span class="input-group-addon input-group-addon-contact">Code Postal</span>
                                 <input type="text" name="codePostal" class="form-control" id="codePostal" placeholder="Code Postal" required="">
                             </div>
-                        </div>
-                        <div class="col-md-12">
+ 
                             <div class="input-group input-group-contact">
-                                <span class="input-group-addon input-group-addon-contact">Ville</span>
                                 <input type="text" name="ville" class="form-control" id="ville" placeholder="Ville" required="">
                             </div>
-                        </div>
-                        <div class="col-md-12">
+
                             <div class="input-group input-group-contact">
-                                <span class="input-group-addon input-group-addon-contact">E-Mail</span>
                                 <input type="text" name="email" class="form-control" id="email" placeholder="E-Mail" required="">
                             </div>
-                        </div>
-
+                      
+                    </div>                                     
+                           <textarea id="message" name="message" placeholder="Votre message" rows="4" cols="137"></textarea>
                         
-                        <div class="col-md-24">
-                            <div class="input-group input-group-contact">
-                                <span class="input-group-addon input-group-addon-contact">Message</span>
-                                <input type="textarea" name="message" class="form-control" id="message" placeholder="Votre Message" required="">
-                            </div>
-                        </div>
-                    </div>                   
-
-                    <div class="col-md-12">
-                       
-                       <div class="input-group input-group-contact">
-                           <span class="input-group-addon input-group-addon-contact">Votre Message</span>
-                           <textarea id="message" name="message" rows="4" cols="137"></textarea>
-                           
-                       </div>
-                  
-               </div>
-
                         <button type="submit" id="envoi" class="btn btn-default btn-lg bouton_contact" alt="envoyer" title="envoyer">ENVOYER</button>
                 </div>  
                 
