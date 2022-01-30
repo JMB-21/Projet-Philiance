82db03917e6175f654747ccb66ce6442373b0577
fridom99 <92981202+fridom99@users.noreply.github.com>
2022-01-30 17:16:07 +0100

    changement tableau de bord auditeur

--- src/Views/home/homepageauditeur.php
+++ src/Views/home/homepageauditeur.php
@@ -1,121 +1,144 @@
 <?php
+
 namespace App\Controllers;
+
 use App\Controllers\Controller;
 use App\Models\Model;
-use App\Models\BiblioModel;
-use App\Models\RubrikModel;
 use App\Models\thememodel;
+use App\Models\SupportModel;
+use PDO;
 ?>
 
+<div class="nir">
+    <!-- Navbar -->
 
+    <div class="banere" style="background-image:url(<?= BASEURL ?>assets/img/banere.jpg);">
+        <img src="" alt="">
+        <div class="container">
 
+        </div>
+    </div>
 
+    <div class="listeLeçon">
+        <div class="container">
+            <div>
+                <h1> FORMATION MÉDICO-SOCIAL</h1>
+            </div>
+            <div class="titreLeçon">
+                <h2>Liste des documents</h2>
+            </div>
 
+            <div class="BlocRecherche">
+                <div class="row">
+                    <div class="col-md-6">
+                        <form class="input-group" action="" method="post">
+                            <div class="form-outline">
+                                <input type="rechercher" id="form1" class="form-control" name="criteria" placeholder="Rechercher" />
+                            </div>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </form>
 
-
-   
-<!--
-
-    Auditeurs :
-    - Lecture des documents  
-
--->
-
-
-    <div class="nir">
-                    <div class="titre_contact">
-                        <div class="titreLeçon">
-                        <h2>Tableau de Bord Auditeur</h2>                        </div>
+                    </div>
+                    <div class="col-md-6 btnListe">
+                        <button type="button" class="btn btn-success btn-sm mb-2"><a href="">Inserer</a></button>
                     </div>
                 </div>
-    
-    <div class="grid-docu">liste des Documents</div>
-        <div class="grid-biblio">
-            <div class="grid-item">        
-                Date 
+            </div>
+            <div class="row listeLeçon">
+                <?php
+
+                foreach ($biblio as $biblio) {
+                ?>
+                    <div class="col-sm-4 col-md-4">
+                        <div class="title-container">
+                            <div class="img-pdf">
+                                <?php
+                                $id = $biblio['idsupport'];
+                                $supp = new SupportModel();
+                                $result = $supp->findsupport($id);
+                                $supp = $result->fetch();
+
+                                switch ($supp['typ']) {
+                                    case 'pdf':
+                                ?>
+                                        <a href="editpdf"><img src="<?= BASEURL ?>assets/img/pdf.svg" alt=""></a>
+                                    <?php
+                                        break;
+                                    case 'mp4': ?>
+                                        <a href="editvideo"><img src="<?= BASEURL ?>assets/img/video.jpg" alt=""></a> <?php
+                                                                                                                    break;
+                                                                                                                default:
+
+                                                                                                                    break;
+                                                                                                            }
+
+                                                                                                            if ($supp['typ'] == "pdf") {
+                                                                                                            } else {
+                                                                                                            }
+                                                                                                                    ?>
+                            </div>
+
+                            <div class="title-detail-container">
+                                <div class="titreCour"><a href="supports.php?id=<?= $biblio['idb'] ?>" title="">
+                                        <?php echo $biblio['titre']; ?>
+                                    </a></div>
+
+                                <div class="reference">
+
+                                    <?php echo $supp['nom'] ?>
+
+                                </div>
+
+                                <div class="reference">
+                                    <span class="txtBold">
+                                        <?php echo " Réf : " . $biblio['reference']; ?>
+                                    </span>
+                                </div>
+
+                                <div class="duree">
+                                    <span class="txtBold">
+                                        <?php
+                                        switch ($supp['typ']) {
+                                            case 'pdf':
+                                                echo "Durée du cours : ";
+                                                break;
+                                            case 'mp4':
+                                                echo "Durée de la vidéo : ";
+                                                break;
+                                            default:
+                                                echo "Durée : ";
+                                                break;
+                                        }
+                                        echo $biblio['duree'];
+                                        ?>
+                                        min</span>
+                                </div>
+
+                                <div class="modifSup">
+                                    <form action="edit" method="POST">
+                                        <input id="id" name="id" type="hidden" value=<?= $biblio['idb'] ?>>
+                                        <button type="submit" class="btn btn-success btn-sm mb-2">Modifier</button>
+                                    </form>
+                                    <form action="edit" method="POST">
+                                        <input id="id" name="id" type="hidden" value=<?= $biblio['idb'] ?>>
+                                        <button type="submit" class="btn btn-success btn-sm mb-2">Supprimer</button>
+                                    </form>
+
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
 
-            <div class="grid-item">
-                Theme
-            </div>
-
-            <div class="grid-item">    
-                Rubrique
-            </div>
-
-            <div class="grid-item">
-                Titre 
-            </div>
-
-            <div class="grid-item">
-                Présentation
-            </div>
-
-            <div class="grid-item"> 
-                Support                 
-            </div>      
-
-            <div class="grid-item"> 
-                Chemin                    
-            </div>
-<?php
-
-
-           foreach ($biblio as $biblio) { 
-?>       
-            <div class="grid-item">        
-                <?= $biblio['creatdate']; ?> 
-            </div>
-            
-            <div class="grid-item  ">  
-                <?php 
-                $id= $biblio['idrubrik'];    
-                $mod     =   new RubrikModel();        
-                $res     =   $mod->find('idrubrik',$id);                        
-                $rub    =   $res->fetch();  
-                 
-                $id= $rub['idthem'];   
-                $mod     =   new thememodel();        
-                $res     =   $mod->find('idth',$id);                        
-                $rub    =   $res->fetch();  
-                // echo $rub['them'];  
-                ?> 
-            </div>
-
-            <div class="grid-item  ">    
-                <?php      
-                    $col = array_column($rubrik, 'idrubrik');
-                    $rub = $biblio['idrubrik'];
-                    $row = array_search($rub,$col);                                   
-                    $valrow = $rubrik[$row]; 
-                    // echo $valrow['nom'];                
-                ?> 
-            </div>
-
-            <div class="grid-item  ">
-                <?= $biblio['titre']; ?> 
-            </div>
-
-            <div class="grid-item  ">
-            <?= $biblio['comment']; ?> 
-            </div>
-
-            <div class="grid-item  ">
-                 <!-- <?= $sup['nom']; ?>    -->
-            </div>
-
-            <div class="grid-item  ">
-                <?= $biblio['chem']; ?>                       
-            </div>
-<?php        
-        }
-?>
         </div>
-        <div>recherche de documents</div>
 
+    </div>
 
-
-
-
-
-
+    </html>
